5cd3fc43eee38b20d4af6de62ef807c88b969081
Sahebjot Singh <sahebjot94@gmail.com>
2014-10-02 11:10:32 +0530

    fixed bug, changed flow of program

--- allImageDownloader.py
+++ allImageDownloader.py
@@ -1,4 +1,6 @@
 #! /usr/bin/env python
+
+
 import requests
 from bs4 import BeautifulSoup
 from StringIO import StringIO
@@ -7,38 +9,52 @@ import os
 import urlparse
 
 #global variable path
-
 path = "allDownloadedImages/"
 
+#check if the url is absolute or not
+#relative urls need to be joint to form absolute url
 
 def isAbsolute(url):
 	return bool(urlparse.urlparse(url).netloc)
 
 def downloader():
+	print "create a new folder to store downloaded images? yes or no?"
+	option = raw_input()
+	if option.lower() == 'yes':
+		pathChanger()
 	print "Enter url"
 	url = raw_input()
 	if url=='':
-		quit()
+		downloader()
 	else:
 		html = requests.get(url)
 		soup = BeautifulSoup(html.text)
 		images = soup.find_all('img')
-		print images	
+		numbering = 0
 		for i in images:
-			imageName = i.get('alt')
+			imageName = i.get('alt') or str(numbering)
 			imageUrl = i.get('src')
+			#if url is relative, make it absolute
 			if not isAbsolute(imageUrl):
 				imageUrl = urlparse.urljoin(url,imageUrl)
-			r = requests.get(imageUrl)
-			image = Image.open(StringIO(r.content))
+			print imageUrl
+
+			try:
+				imagePage = requests.get(imageUrl)
+				image = Image.open(StringIO(imagePage.content))
+			except requests.exceptions.RequestException as e:
+				print e
+				quit()
 			if not os.path.exists(path):
 				os.makedirs(path)	
 			try:
 				imageUrl = imageUrl.split('/')
 				imageUrl = imageUrl[len(imageUrl)-1]
-				image.save(path+str(imageName)+'_'+str(imageUrl))
-			except:
-				print "Error on "+ str(imageName)+'_'+str(imageUrl)
+				fileName = str(imageName)[0:5]+'_'+str(imageUrl)[0:]
+				image.save(path+fileName)
+				print fileName +" Downloaded !"
+			except Exception as e:
+				print "Error "+str(e)+" "+fileName
 				continue
 
 def pathChanger():
@@ -51,15 +67,6 @@ def pathChanger():
 		path = path+ '/'	
 
 def main():
-	print "1.Enter url 2.Enter path(default where script is in folder allDownloadedImages/)"
-
-	option = raw_input()
-	if option == '1':
-		downloader()
-	elif option == '2':
-		pathChanger()
-		downloader()
-	else:
-		quit()
+	downloader()
 if __name__ == '__main__':
 	main()
